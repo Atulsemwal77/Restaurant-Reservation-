f73283a364343cb686eeaf2767c4997e0a054fc7
Atul semwal <atulsemwal332@gmail.com>
2025-03-14 20:53:10 +0530

    render link added

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -20,7 +20,7 @@ const Reservation = () => {
     try {
       // "http://localhost:4000/api/v1/reservation/send",
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://restaurant-reservation-na33.onrender.com/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
